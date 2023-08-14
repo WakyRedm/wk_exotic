f1d463b088ec91ae0b09013cc8eb5a4c008ef33d
Waky <77745131+WakyRedm@users.noreply.github.com>
2023-08-14 11:24:41 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,71 +1,558 @@
 Config = {}
 
 
-Config.treespots = {
+Config.bananaspots = {
 
     tree1 = {
-        Pos = {x= 1236.51 , y= -7036.12, z= 41.63}, 
+        Pos = {x= 1123.61 , y= -7028.37, z= 42.09}, 
         picked = false
     },
     tree2 = {
-        Pos = {x= 1246.59 , y= -7058.14 , z= 41.71}, 
+        Pos = {x= 1121.58 , y= -7042.52 , z= 42.51}, 
         picked = false
     },
     tree3 = {
-        Pos = {x= 1244.83 , y= -7062.08 , z= 41.71}, 
+        Pos = {x= 1114.55 , y= -7039.33 , z= 42.9}, 
         picked = false
     },
     tree4 = {
-        Pos = {x= 1244.06 , y= -7064.9 , z= 41.82}, 
+        Pos = {x= 1103.71 , y= -7026.58 , z= 45.38}, 
         picked = false
     },
     tree5 = {
-        Pos = {x= 1141.73 , y= -7072.12 , z= 45.2}, 
+        Pos = {x= 1124.78 , y= -7049.47 , z= 44.67}, 
         picked = false
     },
     tree6 = {
-        Pos = {x= 1266.03 , y= -7109.5 , z= 41.99}, 
+        Pos = {x= 1131.41 , y= -7071.28 , z= 45.38}, 
         picked = false
     },
     tree7 = {
-        Pos = {x= 1234.32 , y= -7139.38 , z= 44.94}, 
+        Pos = {x= 1148.79 , y= -7102.52 , z= 47.32}, 
         picked = false
     },
     tree8 = {
-        Pos = {x= 1194.72 , y= -7098.05 , z= 43.31}, 
+        Pos = {x= 1264.11 , y= -7091.69 , z= 42.17}, 
         picked = false
     },
     tree9 = {
-        Pos = {x= 1192.66, y= -7093.87 , z= 43.21}, 
+        Pos = {x= 1269.99, y= -7018.85 , z= 43.92}, 
         picked = false
     },
     tree10 = {
-        Pos = {x= 1172.51, y= -7068.43 , z= 43.49}, 
+        Pos = {x= 1299.49, y= -6983.42 , z= 49.21}, 
         picked = false
     },
     tree11 = {
-        Pos = {x= 1120.52 , y= -7038.16 , z= 41.76}, 
+        Pos = {x= 1300.6, y= -6981.45, z= 49.35}, 
         picked = false
     },
     tree12 = {
-        Pos = {x= 1123.95 , y= -7028.56 , z= 42.1}, 
+        Pos = {x= 1307.7 , y= -7013.43 , z= 52.08}, 
+        picked = false
+    }
+}
+Config.cocospots = {
+
+    tree1 = {
+        Pos = {x= 1269.86 , y= -6914.66, z= 44.02}, 
+        picked = false
+    },
+    tree2 = {
+        Pos = {x= 1268.19 , y= -6913.26 , z= 44.03}, 
+        picked = false
+    },
+    tree3 = {
+        Pos = {x= 1280.78 , y= -6935.44 , z= 45.21}, 
+        picked = false
+    },
+    tree4 = {
+        Pos = {x= 1274.48 , y= -6951.53 , z= 44.17}, 
+        picked = false
+    },
+    tree5 = {
+        Pos = {x= 1277.49 , y= -6958.97 , z= 44.95}, 
+        picked = false
+    },
+    tree6 = {
+        Pos = {x= 1278.58 , y= -6960.13 , z= 44.95}, 
+        picked = false
+    },
+    tree7 = {
+        Pos = {x= 1261.5 , y= -6957.89 , z= 44.38}, 
+        picked = false
+    },
+    tree8 = {
+        Pos = {x= 1262.77 , y= -6979.76 , z= 45.56}, 
+        picked = false
+    },
+    tree9 = {
+        Pos = {x= 1258.49, y= -6982.04 , z= 45.17}, 
+        picked = false
+    },
+    tree10 = {
+        Pos = {x= 1282.99, y= -6998.44 , z= 48.86}, 
+        picked = false
+    },
+    tree11 = {
+        Pos = {x= 1253.71, y= -7024.49 , z= 42.25}, 
+        picked = false
+    },
+    tree12 = {
+        Pos = {x= 1236.84, y= -7036.02 , z= 41.57}, 
         picked = false
     },
     tree13 = {
-        Pos = {x= 1125.69 , y= -7022.48 , z= 42.29}, 
+        Pos = {x= 1236.07, y= -7038.2 , z= 41.54}, 
         picked = false
     }
 
 
 }
+Config.manmelspots = {
 
-Config.fruit = {
-
-    'banana',
-    'kiwi',
-    'lemon',
-    'mango',
-    'melon',
-    'pineapple',
-    'orange'
+    tree1 = {
+        Pos = {x= 1117.23 , y= -7080.82, z= 48.45}, 
+        picked = false
+    },
+    tree2 = {
+        Pos = {x= 1112.6, y= -7088.23, z= 49.14}, 
+        picked = false
+    },
+    tree3 = {
+        Pos = {x= 1118.82, y= -7091.45, z= 48.63}, 
+        picked = false
+    },
+    tree4 = {
+        Pos = {x= 1123.15, y= -7084.44, z= 47.92}, 
+        picked = false
+    },
+    tree5 = {
+        Pos = {x= 1130.77, y= -7090.15, z= 46.33}, 
+        picked = false
+    },
+    tree6 = {
+        Pos = {x= 1124.76, y= -7094.96, z= 48.15}, 
+        picked = false
+    }
 }
+Config.ananasspots = {
+
+    tree1 = {
+        Pos = {x= 1324.74 , y= -6944.21, z= 58.78}, 
+        picked = false
+    },
+    tree2 = {
+        Pos = {x= 1323.14, y= -6945.19, z= 58.86}, 
+        picked = false
+    },
+    tree3 = {
+        Pos = {x= 1321.34, y= -6946.10, z= 58.92}, 
+        picked = false
+    },
+    tree4 = {
+        Pos = {x= 1320.18, y= -6944.33, z= 58.54}, 
+        picked = false
+    },
+    tree5 = {
+        Pos = {x= 1321.86, y= -6943.07, z= 58.47}, 
+        picked = false
+    },
+    tree6 = {
+        Pos = {x= 1323.4, y= -6942.07, z= 58.48}, 
+        picked = false
+    },
+    tree7 = {
+        Pos = {x= 1322.02, y= -6939.79, z= 57.93}, 
+        picked = false
+    },
+    tree8 = {
+        Pos = {x= 1320.25, y= -6940.91, z= 57.94}, 
+        picked = false
+    },
+    tree9 = {
+        Pos = {x= 1318.8, y= -6941.95, z= 57.96}, 
+        picked = false
+    },
+    tree10 = {
+        Pos = {x= 1317.45, y= -6940.05, z= 57.4}, 
+        picked = false
+    },
+    tree11 = {
+        Pos = {x= 1319.18, y= -6939.0, z= 57.44}, 
+        picked = false
+    },
+    tree12 = {
+        Pos = {x= 1320.73, y= -6937.97, z= 57.43}, 
+        picked = false
+    }
+}
+Config.kiwispots = {
+
+    tree1 = {
+        Pos = {x= 1138.8 , y= -7048.03, z= 42.21}, 
+        picked = false
+    },
+    tree2 = {
+        Pos = {x= 1137.53, y= -7046.58, z= 42.36}, 
+        picked = false
+    },
+    tree3 = {
+        Pos = {x= 1135.78, y= -7044.8, z= 42.5}, 
+        picked = false
+    },
+    tree4 = {
+        Pos = {x= 1133.5, y= -7042.54, z= 42.57}, 
+        picked = false
+    },
+    tree5 = {
+        Pos = {x= 1135.35, y= -7040.95, z= 42.57}, 
+        picked = false
+    },
+    tree6 = {
+        Pos = {x= 1137.06, y= -7043.3, z= 42.49}, 
+        picked = false
+    },
+    tree7 = {
+        Pos = {x=1138.72, y= -7044.93, z= 42.}, 
+        picked = false
+    },
+    tree8 = {
+        Pos = {x= 1140.16, y= -7046.5, z= 42.2}, 
+        picked = false
+    },
+    tree9 = {
+        Pos = {x= 1141.75, y= -7044.85, z= 42.17}, 
+        picked = false
+    },
+    tree10 = {
+        Pos = {x= 1140.38, y= -7043.44, z= 42.3}, 
+        picked = false
+    },
+    tree11 = {
+        Pos = {x= 1138.71, y= -7041.59, z= 42.47}, 
+        picked = false
+    },
+    tree12 = {
+        Pos = {x= 1136.97, y= -7039.52, z= 42.56}, 
+        picked = false
+    }
+}
+Config.appearspots = {
+
+    tree1 = {
+        Pos = {x= 1069.53 , y= -1817.59, z= 48.91}, 
+        picked = false
+    },
+    tree2 = {
+        Pos = {x= 1079.21 , y= -1816.94, z= 48.91}, 
+        picked = false
+    },
+    tree3 = {
+        Pos = {x= 1088.8 , y= -1816.50, z= 47.67}, 
+        picked = false
+    },
+    tree3 = {
+        Pos = {x= 1088.65 , y= -1806.11, z= 48.33}, 
+        picked = false
+    },
+    tree5 = {
+        Pos = {x= 1078.84 , y= -1806.90, z= 48.78}, 
+        picked = false
+    },
+    tree6 = {
+        Pos = {x= 1068.82 , y= -1806.79, z= 49.31}, 
+        picked = false
+    },
+    tree7 = {
+        Pos = {x= 1061.19 , y= -1797.08, z= 48.4}, 
+        picked = false
+    },
+    tree8 = {
+        Pos = {x= 1068.03 , y= -1796.64, z= 48.8}, 
+        picked = false
+    },
+    tree9 = {
+        Pos = {x= 1078.11 , y= -1796.97, z= 49.11}, 
+        picked = false
+    },
+    tree10 = {
+        Pos = {x= 1088.81 , y= -1796.99, z= 48.56}, 
+        picked = false
+    },
+    tree11 = {
+        Pos = {x= 1088.7 , y= -1787.46, z= 48.98}, 
+        picked = false
+    },
+    tree12 = {
+        Pos = {x= 1079.27 , y= -1787.7, z= 49.19}, 
+        picked = false
+    },
+    tree13 = {
+        Pos = {x= 1068.35 , y= -1787.36, z= 48.64}, 
+        picked = false
+    },
+    tree14 = {
+        Pos = {x= 1060.48 , y= -1787.82, z= 48.11}, 
+        picked = false
+    },
+    tree15 = {
+        Pos = {x= 1051.19, y= -1779.12, z= 47.25}, 
+        picked = false
+    },
+    tree17 = {
+        Pos = {x= 1060.96, y= -1779.28, z= 47.67}, 
+        picked = false
+    },
+    tree18 = {
+        Pos = {x= 1068.68, y= -1778.06, z= 48.36}, 
+        picked = false
+    },
+    tree19 = {
+        Pos = {x= 1078.88, y= -1777.81, z= 49.47}, 
+        picked = false
+    },
+    tree20 = {
+        Pos = {x= 1087.81, y= -1777.75, z= 49.31}, 
+        picked = false
+    },
+    tree21 = {
+        Pos = {x= 1089.01, y= -1768.09, z= 49.49}, 
+        picked = false
+    },
+    tree22 = {
+        Pos = {x= 1078,47, y= -1768.98, z= 49.25}, 
+        picked = false
+    },
+    tree23 = {
+        Pos = {x= 1069.07, y= -1768.95, z= 48.24}, 
+        picked = false
+    },
+    tree24 = {
+        Pos = {x= 1060.42, y= -1769.51, z= 47.57}, 
+        picked = false
+    },
+    tree25 = {
+        Pos = {x= 1050.49, y= -1769.44, z= 47.04}, 
+        picked = false
+    },
+    tree26 = {
+        Pos = {x= 1051.23, y= -1759.32, z= 47.03}, 
+        picked = false
+    },
+    tree27 = {
+        Pos = {x= 1051.76, y= -1750.55, z= 47.07}, 
+        picked = false
+    },
+    tree28 = {
+        Pos = {x= 1060.68, y= -1750.55, z= 47.07}, 
+        picked = false
+    },
+    tree29 = {
+        Pos = {x= 1068.42, y= -1749.28, z= 47.92}, 
+        picked = false
+    },
+    tree30 = {
+        Pos = {x= 1078.03, y= -1749.09, z= 48.96}, 
+        picked = false
+    },
+    tree31 = {
+        Pos = {x= 1087.62, y= -1749.26, z= 49.4}, 
+        picked = false
+    },
+    tree32 = {
+        Pos = {x= 1088.17, y= -1740.61, z= 50.21}, 
+        picked = false
+    },
+    tree33 = {
+        Pos = {x= 1078.49, y= -1741.03, z= 49.24}, 
+        picked = false
+    },
+    tree34 = {
+        Pos = {x= 1070.67, y= -1740.77, z= 48.43}, 
+        picked = false
+    },
+    tree35 = {
+        Pos = {x= 1060.45, y= -1741.29, z= 47.42}, 
+        picked = false
+    },
+    tree36 = {
+        Pos = {x= 1050.39, y= -1741.81, z= 46.95}, 
+        picked = false
+    },
+    tree37 = {
+        Pos = {x= 1051.34, y= -1732.52, z= 46.77}, 
+        picked = false
+    },
+    tree38 = {
+        Pos = {x= 1059.83, y= -1732.98, z= 47.23}, 
+        picked = false
+    },
+    tree39 = {
+        Pos = {x= 1069.27, y= -1732.32, z= 48.17}, 
+        picked = false
+    },
+    tree40 = {
+        Pos = {x= 1078.64, y= -1732.02, z= 49.16}, 
+        picked = false
+    },
+    tree41 = {
+        Pos = {x= 1087.39, y= -1731.82, z= 50.11}, 
+        picked = false
+    },
+    tree42 = {
+        Pos = {x= 1087.79, y= -1722.42, z= 50.48}, 
+        picked = false
+    },
+    tree43 = {
+        Pos = {x= 1077.63, y= -1722.63, z= 49.53}, 
+        picked = false
+    },
+    tree44 = {
+        Pos = {x= 1068.49, y= -1722.71, z= 48.21}, 
+        picked = false
+    },
+    tree45 = {
+        Pos = {x= 1058.45, y= -1723.46, z= 47.44}, 
+        picked = false
+    },
+    tree46 = {
+        Pos = {x= 1068.34, y= -1712.90, z= 48.39}, 
+        picked = false
+    },
+    tree47 = {
+        Pos = {x= 1077.82, y= -1712.94, z= 49.45}, 
+        picked = false
+    },
+}
+Config.citrusspots = {
+    tree1 = {
+        Pos = {x= 2035.62 , y= -835.55, z= 41.78}, 
+        picked = false
+    },
+    tree2 = {
+        Pos = {x= 2035.62 , y= -845.5, z= 41.95}, 
+        picked = false
+    },
+    tree3 = {
+        Pos = {x= 2035.57 , y= -855.32, z= 42.3}, 
+        picked = false
+    },
+    tree4 = {
+        Pos = {x= 2035.13 , y= -864.93, z= 42.3}, 
+        picked = false
+    },
+    tree5 = {
+        Pos = {x= 2026.39 , y= -866.05, z= 42.16}, 
+        picked = false
+    },
+    tree6 = {
+        Pos = {x= 2026.63 , y= -855.98, z= 41.79}, 
+        picked = false
+    },
+    tree7 = {
+        Pos = {x= 2026.49 , y= -845.31, z= 42.91}, 
+        picked = false
+    },
+    tree8 = {
+        Pos = {x= 2026.47 , y= -835.37, z= 41.91}, 
+        picked = false
+    },
+    tree8 = {
+        Pos = {x= 2026.47 , y= -826.52, z= 41.23}, 
+        picked = false
+    },
+    tree9 = {
+        Pos = {x= 2016.87 , y= -827.64, z= 41.99}, 
+        picked = false
+    },
+    tree10 = {
+        Pos = {x= 2016.82 , y= -835.71, z= 41.95}, 
+        picked = false
+    },
+    tree11 = {
+        Pos = {x= 2016.86 , y= -845.64, z= 42.28}, 
+        picked = false
+    },
+    tree12 = {
+        Pos = {x= 2017.22 , y= -854.51, z= 42.61}, 
+        picked = false
+    },
+    tree13 = {
+        Pos = {x= 2017.08 , y= -866.0, z= 42.3}, 
+        picked = false
+    },
+    tree14 = {
+        Pos = {x= 2017.54 , y= -873.78, z= 42.66}, 
+        picked = false
+    },
+    tree15 = {
+        Pos = {x= 2009.04, y= -873.30, z= 42.13}, 
+        picked = false
+    },
+    tree16 = {
+        Pos = {x= 2007.73, y= -865.62, z= 42.7}, 
+        picked = false
+    },
+    tree17 = {
+        Pos = {x= 2007.41, y= -855.39, z= 42.7}, 
+        picked = false
+    },
+    tree18 = {
+        Pos = {x= 2007.29, y= -846.49, z= 42.81}, 
+        picked = false
+    },
+    tree19 = {
+        Pos = {x= 2007.28, y= -836.55, z= 41.6}, 
+        picked = false
+    },
+    tree20 = {
+        Pos = {x= 2007.27, y= -826.71, z= 41.43}, 
+        picked = false
+    },
+
+    ----------the rest is too much for me, if you want you do the rest :)
+}
+
+--------------------------------fruits------------------
+Config.banana = {
+    'banana'
+}
+Config.coconut = {
+    'coconut'
+}
+Config.manmel = {
+    'mango',
+    'melon'
+}
+Config.kiwi = {
+    'kiwi'
+}
+Config.ananas = {
+    'pineapple'
+}
+Config.appear = {
+    'apple',
+    'pear'
+}
+Config.citrus = {
+    'orange',
+    'lemon'
+}
+
+
+
+
+
+--Config.fruit = {
+    --'coconut',          ---1guarma big tree                 random V
+    --'banana',           ---1 guarma                         random V
+    --'kiwi',             ---1 guarma rdr_bush_palm_aa_sim    1136.83, -7045.29, 42.44
+    --'lemon',            ----2 Bayou
+    --'mango',            ---1 guarma p_tree_mesquite_01      1121.05, -7088.13, 48.27
+    --'melon',            ---1 guarma p_tree_mesquite_01      1121.05, -7088.13, 48.27
+    --'pineapple',        ---1 guarma rdr_bush_aloe_aa_sim    1320.5, -6942.72, 58.59 blip
+    --'orange',           ---2 Bayou
+    --'apple',            ---3 Braith
+    --'pear'              --3 Braith
+--}
