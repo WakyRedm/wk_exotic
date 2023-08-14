fce4e36d8298d60a842124a464159a76031f1068
Waky <77745131+WakyRedm@users.noreply.github.com>
2023-08-14 11:25:19 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -8,18 +8,93 @@ end)
 
 VORPinv = exports.vorp_inventory:vorp_inventoryApi()
 
+RegisterServerEvent("wk_exotic:dropcoconut")
+AddEventHandler("wk_exotic:dropcoconut", function()
+    local _source = source 
+    local User = VORPcore.getUser(_source)
+    local Character = User.getUsedCharacter
+    local drop = Config.coconut
 
-RegisterServerEvent("wk_exotic:fruits")
-AddEventHandler("wk_exotic:fruits", function()
+    VORPinv.addItem(_source, drop, 1)
+    TriggerClientEvent('vorp:TipRight', _source, "You found 1 ~d~Coconut .")
+end)
+
+RegisterServerEvent("wk_exotic:dropkiwi")
+AddEventHandler("wk_exotic:dropkiwi", function()
+    local _source = source 
+    local User = VORPcore.getUser(_source)
+    local Character = User.getUsedCharacter
+    local drop = Config.kiwi[math.random(1, #Config.kiwi)]
+    local amount = math.random(1, 2)
+
+    VORPinv.addItem(_source, drop, amount)
+    TriggerClientEvent('vorp:TipRight', _source, "You found " .. amount .. " ~t6~Kiwi's~q~.")
+end)
+
+RegisterServerEvent("wk_exotic:dropbanana")
+AddEventHandler("wk_exotic:dropbanana", function()
+    local _source = source 
+    local User = VORPcore.getUser(_source)
+    local Character = User.getUsedCharacter
+    local drop = Config.banana
+    local amount = math.random(2, 4)
+
+    VORPinv.addItem(_source, drop, amount)
+    TriggerClientEvent('vorp:TipRight', _source, "You found " .. amount .. " ~o~Bananas~q~.")
+end)
+
+RegisterServerEvent("wk_exotic:dropananas")
+AddEventHandler("wk_exotic:dropananas", function()
+    local _source = source 
+    local User = VORPcore.getUser(_source)
+    local Character = User.getUsedCharacter
+    local drop = Config.ananas
+
+    VORPinv.addItem(_source, drop, 1)
+    TriggerClientEvent('vorp:TipRight', _source, "You found 1 ~o~Pineapple~q~.")
+end)
+
+RegisterServerEvent("wk_exotic:melonmango")
+AddEventHandler("wk_exotic:melonmango", function()
     local _source = source
     local User = VORPcore.getUser(_source) 
     local Character = User.getUsedCharacter         
-    local r = Config.fruit[math.random(1, #Config.fruit)]
+    local r = Config.manmel[math.random(1, #Config.manmel)]
     local amount = math.random(1, 3)
         
     VORPinv.addItem(_source, r, amount)
         
-    local player = Character.source
+    --local player = Character.source
+         
+    TriggerClientEvent("vorp:TipRight", _source, "You found " .. amount .. "  ~t4~" .. r .. " ~q~.", 5000) 
+end)
+
+RegisterServerEvent("wk_exotic:appear")
+AddEventHandler("wk_exotic:appear", function()
+    local _source = source
+    local User = VORPcore.getUser(_source) 
+    local Character = User.getUsedCharacter         
+    local r = Config.appear[math.random(1, #Config.appear)]
+    local amount = math.random(1, 3)
         
-    TriggerClientEvent("vorp:TipRight", _source, "You found " .. amount .. " " .. r .. ".", 5000) 
+    VORPinv.addItem(_source, r, amount)
+        
+    --local player = Character.source
+         
+    TriggerClientEvent("vorp:TipRight", _source, "You found " .. amount .. "  ~t4~" .. r .. " ~q~.", 5000) 
+end)
+
+RegisterServerEvent("wk_exotic:citrus")
+AddEventHandler("wk_exotic:citrus", function()
+    local _source = source
+    local User = VORPcore.getUser(_source) 
+    local Character = User.getUsedCharacter         
+    local r = Config.citrus[math.random(1, #Config.citrus)]
+    local amount = math.random(1, 3)
+        
+    VORPinv.addItem(_source, r, amount)
+        
+    --local player = Character.source
+         
+    TriggerClientEvent("vorp:TipRight", _source, "You found " .. amount .. "  ~t4~" .. r .. " ~q~.", 5000) 
 end)
