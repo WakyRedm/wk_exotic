8c7285335c363f8ab1c32643f90b3ad39a354910
Waky <77745131+WakyRedm@users.noreply.github.com>
2023-08-14 11:24:59 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-author 'Waky    discord:waky'
+author 'Waky --#discord:waky'
 game 'rdr3'
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
